a193e16ea373d5e30be06e4150e10c7e7470df97
David Welborn <dswelbor@asu.edu>
2020-03-27 13:37:10 -0400

    Add logic for list of hi comment submissions

--- consumer.py
+++ consumer.py
@@ -7,13 +7,13 @@ import praw
 
 # parameters
 submission_qty = 100  # Number of posts to pull from api
-comments_qty = 1000  # Target number of comments
+comments_threshold = 1000  # Target number of comments
 
 # Initialize python reddit api wrapper for r/popular subreddit
 reddit = praw.Reddit()
 popular = reddit.subreddit('popular')
 
-# Get top 100 hot posts
+# Get top 100 hot submissions
 hot_submissions = popular.hot(limit=submission_qty)
 # Populate list of OC content
 oc_submissions = []  # hot_submissions.filters.add(is_original_content=True)
@@ -21,6 +21,13 @@ for submission in hot_submissions:
     if submission.is_original_content:
         oc_submissions.append(submission)
 
+# Get highly comment submissions
+hot_submissions = popular.hot(limit=submission_qty)
+hi_comment_submissions = []
+for submission in hot_submissions:
+    print(submission.num_comments)
+    if submission.num_comments > comments_threshold:
+        hi_comment_submissions.append(submission)
 
-print(len(oc_submissions))
-
+print(f'Number of OC submission: {len(oc_submissions)}')
+print(f'Number of highly commented submissions: {len(hi_comment_submissions)}')
