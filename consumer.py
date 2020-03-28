cc9beecc978cd57d264be0943da17b3002af4d89
David Welborn <dswelbor@asu.edu>
2020-03-28 17:47:17 -0400

    Update whitespace and docstring comments for Consumer

--- consumer.py
+++ consumer.py
@@ -1,12 +1,13 @@
 """
-This script consumes data from the reddit api and returns that 
-deserialized data
+This module defines and implements class(es) to provide functionality for 
+consuming the reddit api.
 """
 
 import praw
 from models import Forum, Post
 
 class Consumer:
+    """A class to implement api consumption functionality"""
     # parameters
     QTY = 100  # Number of posts to pull from api
     COMMENT_THRESHOLD = 1000  # Target number of comments
@@ -14,6 +15,7 @@ class Consumer:
     MULTIREDDIT_NAME = 'top100recurring'
 
     def __init__(self):
+        """Simple constructor with defaults from praw config"""
         # Initialize python reddit api wrapper for r/popular subreddit
         self.reddit = praw.Reddit()
         self.popular = self.reddit.subreddit('popular')
@@ -151,85 +153,3 @@ class Consumer:
         # Create a multireddit with recurring subreddits
         self.reddit.multireddit.create(display_name=self.MULTIREDDIT_NAME, 
            subreddits=recurring_subreddits)
-
-"""
-# parameters
-SUBMISSION_QTY = 100  # Number of posts to pull from api
-COMMENT_THRESHOLD = 1000  # Target number of comments
-
-# Initialize python reddit api wrapper for r/popular subreddit
-reddit = praw.Reddit()
-popular = reddit.subreddit('popular')
-# Get top 100 hot submissions
-hot_submissions = list(popular.hot(limit=SUBMISSION_QTY))
-
-# Populate list of OC content
-oc_submissions = []
-for submission in hot_submissions:
-    if submission.is_original_content:
-        oc_submissions.append(submission)
-
-# Get highly commented submissions
-hi_comment_submissions = []
-for submission in hot_submissions:
-    if submission.num_comments > COMMENT_THRESHOLD:
-        hi_comment_submissions.append(submission)
-
-# Get upvoted submissions descending
-upvoted_submissions = hot_submissions[:] # create a shallow copy of list
-# Sort hot submissions by upvote score
-upvoted_submissions.sort(key=lambda sub: getattr(sub, 'score'), reverse=True)
-top10_upvoted_submissions = upvoted_submissions[0:10] # Get top10 list
-
-# Get unique subreddits
-unique_subreddits = []
-for submission in hot_submissions:
-    # print(submission.subreddit)
-    if submission.subreddit not in unique_subreddits:
-        unique_subreddits.append(submission.subreddit)
-"""
-"""
-# Get recurring subreddits
-seen_subreddits = {}  # dictionary for seen subreddits
-recurring_subreddits = []
-for submission in hot_submissions:
-    try:
-        # This subredit has already been seen at least once
-        seen_subreddits[submission.subreddit] += 1 # increment count
-
-        if seen_subreddits[submission.subreddit] == 2:
-            # First time subreddit has been seen more than once
-            recurring_subreddits.append(submission.subreddit) # add to list
-
-    except KeyError:
-        # First time seeing this subreddit
-        seen_subreddits[submission.subreddit] = 1
-
-# Create a multireddit with recurring subreddits
-# new_multireddit = reddit.multireddit.create(display_name='testing1236', 
-#    subreddits=recurring_subreddits)
-
-
-"""
-"""
-# print summaries
-print(f'Number of OC submission: {len(oc_submissions)}')
-print(f'Number of highly commented submissions: {len(hi_comment_submissions)}')
-print(f'Number of top 10 hot submissions: {len(top10_upvoted_submissions)}')
-for test in top10_upvoted_submissions:
-    print(test.score)
-print(f'Number of unique subreddits in hot submissions: {len(unique_subreddits)}')
-"""
-# print(f'Number of recurring subreddits in hot submissions: {len(recurring_subreddits)}')
-# print(new_multireddit)
-
-"""
-## Create a list of Posts
-test_posts = []
-for post in top10_upvoted_submissions:
-    test_posts.append(Post(title=post.title, url=post.permalink, upvotes=post.score, comment_count=post.num_comments))
-
-print('test')
-for test_post in test_posts:
-    print(f'title: {test_post.title} \nurl: {test_post.url} \nupvotes: {test_post.upvotes} \ncomments: {test_post.comment_count}')
-"""
